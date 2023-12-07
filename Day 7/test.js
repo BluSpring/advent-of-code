9ebbc120411a9f5307487de9182293dcb979c5c3
Naz <bluspringindohq@gmail.com>
2023-12-08 05:50:11 +0800

    Day 7

---
+++ Day 7/test.js
@@ -0,0 +1,94 @@
+const start = Date.now();
+
+const fs = require('fs');
+const test = fs.readFileSync('./test.txt').toString();
+const testValue = 6592;
+let sum = 0;
+
+const strengths = 'AKQJT98765432'.split('');
+const lines = test.split('\n');
+
+let types = {};
+let bids = {};
+
+/**
+ * 
+ * @param {string} hand 
+ */
+function scanMatches(hand) {
+    let totals = {};
+
+    for (const char of hand) {
+        if (!totals[char])
+            totals[char] = 0;
+
+        totals[char]++;
+    }
+
+    return totals;
+}
+
+function result(map, index) {
+    return map[Object.keys(map)[index]];
+}
+
+for (const line of lines) {
+    if (line.trim() == '')
+        continue;
+
+    const split = line.split(' ');
+    const hand = split[0];
+    const bid = split[1];
+    bids[hand] = bid;
+
+    const matches = scanMatches(hand);
+
+    if (result(matches, 0) == 5) { // five of a kind
+        types[hand] = 6;
+    } else if (result(matches, 0) == 4 || result(matches, 1) == 4) { // four of a kind
+        types[hand] = 5;
+    } else if ((result(matches, 0) == 3 && result(matches, 1) == 2) || (result(matches, 0) == 2 && result(matches, 1) == 3)) { // full house
+        types[hand] = 4;
+    } else if (result(matches, 0) == 3 || result(matches, 1) == 3 || result(matches, 2) == 3) { // three of a kind
+        types[hand] = 3;
+    } else if ((result(matches, 0) == 2 && result(matches, 1) == 2) || (result(matches, 1) == 2 && result(matches, 2) == 2) || (result(matches, 0) == 2 && result(matches, 2) == 2)) { // two pair
+        types[hand] = 2;
+    } else if ((result(matches, 0) == 2) || (result(matches, 1) == 2) || (result(matches, 2) == 2) || (result(matches, 3) == 2) || (result(matches, 4) == 2)) { // one pair
+        types[hand] = 1;
+    } else {
+        types[hand] = 0;
+    }
+}
+
+function isStronger(source, compare) {
+    for (let i = 0; i < 5; i++) {
+        if (strengths.indexOf(source[i]) < strengths.indexOf(compare[i]))
+            return true;
+        else if (strengths.indexOf(source[i]) > strengths.indexOf(compare[i]))
+            return false;
+    }
+
+    return false;
+}
+
+const sortedHands = Object.keys(types).sort((a, b) => {
+    if (types[a] > types[b])
+        return 1;
+    else if (types[b] > types[a])
+        return -1;
+    else if (isStronger(a, b))
+        return 1;
+    else if (isStronger(b, a))
+        return -1;
+    else
+        return 0;
+});
+
+for (let i = 0; i < sortedHands.length; i++) {
+    const hand = sortedHands[i];
+    const bid = bids[hand];
+    sum += bid * (i + 1);
+}
+
+console.log(`sum: ${sum} (${sum == testValue})`);
+console.log(`took ${Date.now() - start}ms`);
